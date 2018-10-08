c5940933ec1511302967ab41a2c037c67e776747
cankert <ankert.c@gmail.com>
2018-10-08 16:26:21 +0200

    Get User Input instead of fixed Variable
    Signed-off-by: cankert <ankert.c@gmail.com>

--- src/romanConverterTestDrive.java
+++ src/romanConverterTestDrive.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class romanConverterTestDrive {
     public static void main (String[] args){
 
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please put in the Number you want translated:");
+
+        int userInput = reader.nextInt();
+        reader.close();
         //Simulate User Input
-        int userInput = 85;
+
         // Initialize emtpy result
         String result;
 
         //call method to convert int to roman number
 
-        result = romanConverter.convert(userInput);
+        result = "Your Number corresponds to the Roman Number: " + romanConverter.convert(userInput);
 
 
         // Print out Result
